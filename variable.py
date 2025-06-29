81ab558e01199c2e76fea7b25301fac5c13cc38f
dindinds2 <makarovtarkis@gmail.com>
2025-06-29 19:59:37 +0700

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,8 @@
+Name = "Dindin"
+age = 14
+print("My name is",Name)
+print("My age is",age)
+School = input("What is your school name: ")
+print("My school is",School)
+Birthday = int(input("What is your birthday: "))
+print("My birthday is",Birthday)
