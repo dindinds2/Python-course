5265735888115bc3934f19c96206923222dbddda
dindinds2 <makarovtarkis@gmail.com>
2025-06-30 18:26:47 +0700

    Add files via upload

---
+++ ACPbirthday.py
@@ -0,0 +1,10 @@
+friend =  "4/5/2011"
+print("My friend birthday is",friend)
+friend2 =  "12/6/2010"
+print("My friend birthday is",friend2)
+friend3 =  "11/1/2011"
+print("My friend birthday is",friend3)
+friend4 =  "14/10/2009"
+print("My friend birthday is",friend4)
+friend5 =  "1/1/2011"
+print("My friend birthday is",friend5)
