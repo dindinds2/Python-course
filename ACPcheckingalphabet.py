eb2f890a1cdbbcae8f9b97a37ad9225e9b8ef9ca
dindinds2 <makarovtarkis@gmail.com>
2025-07-11 11:57:53 +0700

    Add files via upload

---
+++ ACPcheckingalphabet.py
@@ -0,0 +1,5 @@
+text = (input("Enter your charecter: "))
+if text.lower() in ["a","b","c","d","e","f",'g',"h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]:
+    print("This text is character")
+else:
+    print("This text is number")
