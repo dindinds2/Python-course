9dcc8ca1f34f24fe31a11d2cd40fad25e7acd26a
dindinds2 <makarovtarkis@gmail.com>
2025-07-19 13:38:12 +0700

    Add files via upload

---
+++ Reversenumber.py
@@ -0,0 +1,4 @@
+Number = int(input("Enter the number that will reversed: "))
+print("Number from {0} to {1} are".format(Number,1))
+for i in range(Number,0,-1):
+    print(i)
---
+++ reverseastring.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+string1 = ('')
+for i in name:
+    string1 = i+string1
+print("The reversed name is",string1)
