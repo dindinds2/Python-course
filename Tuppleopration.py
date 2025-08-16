2e997543ff0d1a9ad98446ce60f05ad6d47b94b7
dindinds2 <makarovtarkis@gmail.com>
2025-08-16 13:42:21 +0700

    Add files via upload

---
+++ Tuppleopration.py
@@ -0,0 +1,12 @@
+
+tuple = ("Dindin",14,170.5,True)
+print(tuple)
+tupleint = (1,2,3,4,5,6)
+print(tupleint)
+tupleint = tupleint + (9,)
+print(tupleint)
+tupleoccur = (2,2,6,6,7,8,9)
+print(tupleoccur.count(6))
+slicetuple = ('D','i','n','d','i','n')
+_slice = slicetuple[2:4]
+print(_slice)
